7a5ad1042f41011c0637ab6e338cddad97b37cf8
Oleksandr Mysiura <mysiuole@fel.cvut.cz>
2024-05-19 23:49:04 +0200

    Refactor colors.h to use Color565 struct for improved color handling

--- colors.h
+++ colors.h
@@ -1,3 +1,6 @@
+#pragma once
+#include <cstdint>
+
 enum COLORS{
     RED = 0xf800,
     GREEN = 0x07e0,
@@ -8,3 +11,26 @@ enum COLORS{
     PURPLE = 0xf81f,
     CYAN = 0x07ff
 };
+
+typedef struct Color565 {
+    unsigned int blue : 5;
+    unsigned int green : 6;
+    unsigned int red : 5;
+
+    Color565() : blue(0), green(0), red(0) {}
+    Color565(uint16_t color) {
+        blue = (color & 0x001F);
+        green = (color & 0x07E0) >> 5;
+        red = (color & 0xF800) >> 11;
+    }
+    Color565 operator*(int factor) const {
+        Color565 result;
+        result.blue = blue * factor;
+        result.green = green * factor;
+        result.red = red * factor;
+        return result;
+    }
+    operator uint16_t() const {
+        return (red << 11) | (green << 5) | blue;
+    }
+} color565_t;
